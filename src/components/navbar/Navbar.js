6d26750cb00d16f5d2390f62c83a4ced16c24330
vikhulei <vikhulei@gmail.com>
2023-12-24 10:13:52 +0000

    pics

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,8 @@
 import { Wrapper, Upperbar, Middlebar, Bottombar, Logo } from "./NavbarStyle";
-import CMF from "../../assets/CMF.png"
+import CMF from "../../assets/CMF.png";
+import one from "../../assets/01.jpg";
+import two from "../../assets/02.jpg";
+import three from "../../assets/03.jpg";
 
 const Navbar = () => {
     return (
@@ -9,6 +12,9 @@ const Navbar = () => {
                 <Logo src={CMF} />
             </Middlebar>
             <Bottombar></Bottombar>
+            <img src={one} width = "380vw" />
+            <img src={two} width = "380vw" />
+            <img src={three} width = "380vw" />
         </Wrapper>
     )
 }

599c99bc6c2b92a7948f027a67452a0d932b3823
vikhulei <vikhulei@gmail.com>
2024-01-10 22:21:08 +0000

    fonts and colors

---
+++ src/components/01_constants/Colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+ lightgreen: "#00E54E",
+ green: "#79B977",
+ backgroundgreen: "#BCE5AE",
+}
--- src/components/02_navbar/NavbarStyle.js
+++ src/components/02_navbar/NavbarStyle.js
@@ -1,7 +1,9 @@
 import styled from "styled-components"
 import { sizes } from "../01_constants/Sizes"
+import { colors } from "../01_constants/Colors"
 
 const { navbarheight, upperbarheight, bottombarheight, navbarpaddings } = sizes
+const { lightgreen, backgroundgreen } = colors
 
 const Wrapper = styled.div`
     // position: fixed;

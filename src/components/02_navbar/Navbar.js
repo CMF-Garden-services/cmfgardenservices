3efeadb62259e9df4454451ef20ddf2f81dfb869
vikhulei <vikhulei@gmail.com>
2024-01-10 22:23:03 +0000

    cleaned navbar

--- src/components/02_navbar/Navbar.js
+++ src/components/02_navbar/Navbar.js
@@ -1,25 +1,10 @@
-import { Wrapper, Upperbar, Middlebar, Warwickshire, Phone, Bottombar, Logo, Menuitem, Gardening, Button } from "./NavbarStyle";
+import { Wrapper } from "./NavbarStyle";
 import CMF from "../../assets/01_navbar/CMF.png";
 
 const Navbar = () => {
     return (
         <Wrapper>
-            <Upperbar>
-                <Gardening>CMF Garden Services</Gardening>
-                <Button>Request a call back</Button>
-            </Upperbar>
-            <Middlebar>
-                    <Warwickshire>Stratford Upon Avon -  <br/>Solihull</Warwickshire>
-                    <Logo src={CMF} />
-                    <Phone>07873-122257</Phone>
-            </Middlebar>
-            <Bottombar>
-                <Menuitem href="#home">Home</Menuitem>
-                <Menuitem href="#about">About</Menuitem>
-                <Menuitem href="#">Locations</Menuitem>
-                <Menuitem href="#services">Services</Menuitem>
-                <Menuitem href="#">Contacts</Menuitem>
-            </Bottombar>
+
         </Wrapper>
     )
 }

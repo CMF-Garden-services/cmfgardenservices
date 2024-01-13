9a6ca20b79a1169cb1a853b8a3c99d5175a68ac8
vikhulei <vikhulei@gmail.com>
2024-01-13 23:11:44 +0000

    hamburger menu

---
+++ src/components/03_sections/03_locations/Locations.js
@@ -0,0 +1,23 @@
+import { Wrapper, AboutText, Image } from "./LocationsStyle";
+
+const Locations = () => {
+    return (
+        <Wrapper>
+            <AboutText>
+            Hello, my name is Chris. I spent 8 years working for one of the country’s leading lawn care companies
+before setting up CMF Garden services. 
+<br/><br/>
+
+The aim of CMF Garden Services is to provide a reliable and
+friendly high quality garden maintenance service to the local community. 
+<br/><br/>
+
+I am fully insured and have
+PA1 and PA6 licences so you can book my services with peace of mind. I cover all areas between the
+Solihull area and the Stratford upon Avon area.
+            </AboutText>
+        </Wrapper>
+    )
+}
+
+export default Locations

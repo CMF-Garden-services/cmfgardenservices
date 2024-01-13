0cc3a0e3abf3d0bfa21a816dc48bcd30752e3d71
vikhulei <vikhulei@gmail.com>
2024-01-13 11:05:40 +0000

    navbar solid background showing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/02_navbar/Navbar';
 import NavbarBg from './components/02_navbar/NavbarBg';
+import Home from './components/03_sections/01_home/Home';
 
 function App() {
   return (
     <div>
           <Navbar />
           <NavbarBg />
+          <Home />
     </div>
   );
 }

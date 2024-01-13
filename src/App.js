8925e9daf4661d24355fcad447095643ac23346c
vikhulei <vikhulei@gmail.com>
2024-01-13 23:11:32 +0000

    Locations filed renamed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from './components/02_navbar/Navbar';
 import NavbarBg from './components/02_navbar/NavbarBg';
 import Home from './components/03_sections/01_home/Home';
+import About from './components/03_sections/02_about/About'
+import Locations from './components/03_sections/03_locations/Locations';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
           <Navbar />
           <NavbarBg />
           <Home />
+          <About />
+          <Locations />
     </div>
   );
 }

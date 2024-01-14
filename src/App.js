f2e2313b58bb6f4d23faf7749d6b07dcac5f995d
vikhulei <vikhulei@gmail.com>
2024-01-14 17:04:07 +0000

    footer section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/03_sections/01_home/Home';
 import About from './components/03_sections/02_about/About'
 import Locations from './components/03_sections/03_locations/Locations';
 import Services from './components/03_sections/04_services/Services';
+import Footer from './components/03_sections/05_footer/Footer'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <About />
           <Locations />
           <Services />
+          <Footer />
     </div>
   );
 }
